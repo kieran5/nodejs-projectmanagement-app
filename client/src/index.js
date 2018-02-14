31e4a28f1bd0b9debd7b93200c9678574d73024f
kieran5 <kieranwillisx@gmail.com>
2018-02-14 20:26:53 +0000

    Basic React routing setup. Pages to be implemented and connected to backend.

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,8 @@ import './styles/index.css';
 import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'));
+
 registerServiceWorker();
--- client/src/views/App.js
+++ client/src/views/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
+import Login from './Login';
 import Projects from './Projects';
+import Register from './Register';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Projects />
-      </div>
+      <Router>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <NavLink to="/" className="navbar-brand">Project Hub</NavLink>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <NavLink to="/" exact activeClassName="activeNav" className="nav-link">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/login" activeClassName="activeNav" className="nav-link">Login</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/register" activeClassName="activeNav" className="nav-link">Register</NavLink>
+                </li>
+              </ul>
+              <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+
+          <Route exact path="/" component={Projects} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+
+        </div>
+      </Router>
     );
   }
 }
